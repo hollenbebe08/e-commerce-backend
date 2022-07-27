0a64e8d6f5badf479cb84ea55b004b769b62f839
Betsy <betsy.griffith12@gmail.com>
2022-07-27 16:59:08 -0400

    created category and product models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          // this means their needs to be at least four characters in the category
+          len: [4]
+      }
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,48 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means their needs to be at least four characters in the product description
+        len: [4]
+      }
+    },
+    Price: {
+      // decimal
+      type: DataTypes.DECIMAL,
+      // doesn't allow null values
+      allowNull: false,
+      // validates that the value is a decimal
+      validate: {
+        isDecimal: true, 
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //set as default value of ten
+      defaultValue: 10,
+      //validates that the value is numeric
+      validate: {
+        not: ["[a-z]",'i'],
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //reference's the category's model id
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
